48837df00d14a7733da70d3fae42175cb1fe4d24
Hung PC <lamgiahung112@gmail.com>
2024-04-13 10:37:34 +0700

    migrate to local server with ngrok

--- utilities/constants.ts
+++ utilities/constants.ts
@@ -1,6 +1,6 @@
 const tags = ["SLIDING_WINDOW", "MATH", "DYNAMIC_PROGRAMMING", "ARRAY", "STRING"] as const
 const user_challenge_progress_status = ["ATTEMPTED", "SOLVED"] as const
 const difficulties = ["EASY", "MEDIUM", "HARD"] as const
-const base_url = "https://trikode-server.onrender.com"
+const base_url = "https://walleye-fluent-flounder.ngrok-free.app"
 
 export { tags, user_challenge_progress_status, difficulties, base_url }
